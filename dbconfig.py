68fd80ace96f79736a98c3ee5f97cc0e90cbf9b3
Joan Healy <joan@altv.com>
2019-12-15 10:36:31 +0000

    changed db creds to remote

--- dbconfig.py
+++ dbconfig.py
@@ -1,13 +1,13 @@
 mysql = {
   # Local DB configuration
-  'host' : 'localhost',
-  'user' : 'root',
-  'password' : 'Ma1r3ad2015*%',
-  'database' : 'Books'
+  # 'host' : 'localhost',
+  # 'user' : 'root',
+  # 'password' : 'Ma1r3ad2015*%',
+  # 'database' : 'Books'
 
   # Remote DB configuration
-  # 'host' : 'JoanHealy.mysql.pythonanywhere-services.com',
-  # 'user' : 'JoanHealy',
-  # 'password' : 'Ma1r3ad2015*%',
-  # 'database' : 'JoanHealy$Books'
+  'host' : 'JoanHealy.mysql.pythonanywhere-services.com',
+  'user' : 'JoanHealy',
+  'password' : 'Ma1r3ad2015*%',
+  'database' : 'JoanHealy$Books'
 }
